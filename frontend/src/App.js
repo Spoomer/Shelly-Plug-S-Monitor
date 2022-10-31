2225bf6f325f92147231d9d1a59eab3cca33efaa
Spoomer <renerahnfeld@gmail.com>
2022-10-31 12:00:52 +0100

    add chart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import EPower from './Components/EPower';
+import Shelly from './Components/Shelly';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Shelly Stromverbrauch</h2>
+          <h2>Shelly Plug S</h2>
         </div>
         <div className="App-intro">
-          <EPower/>
+          <Shelly/>
         </div>
       </div>
     );
