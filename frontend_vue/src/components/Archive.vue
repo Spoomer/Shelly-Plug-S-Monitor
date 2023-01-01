7d3a46917226d3ff947a58388665b1f12fd5911b
Spoomer <renerahnfeld@gmail.com>
2023-01-01 13:48:27 +0100

    fix archive date input + local datetime display

--- frontend_vue/src/components/Archive.vue
+++ frontend_vue/src/components/Archive.vue
@@ -1,18 +1,43 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
-
+const getDateString = (date?: Date) => { const newDate = date ? new Date(date) : new Date(); return new Date(newDate.getTime() - newDate.getTimezoneOffset() * 60000).toISOString().slice(0, -1); };
 const dataArr: any[] = [];
 const pricePerKwhCache: number = Number.parseFloat(localStorage.getItem("pricePerKwh") ?? "0");
 function changepricePerKwh(event: Event) {
     pricePerKwh = Number.parseFloat((event.target as HTMLInputElement)?.value ?? 0);
     localStorage.setItem("pricePerKwh", pricePerKwh.toString())
 }
+function handleFromInput(event: Event) {
+    let input = Date.parse((event.target as HTMLInputElement)?.value);
+    if (!Number.isNaN(input)) {
+        state.from = new Date(input);
+    }
+}
+function handleToInput(event: Event) {
+    let input = Date.parse((event.target as HTMLInputElement)?.value);
+    if (!Number.isNaN(input)) {
+        state.to = new Date(input);
+    }
+}
 let pricePerKwh: number = pricePerKwhCache;
 
 function getTimeStampInSeconds(date: Date) {
     return (date.getTime()) / 1000;
 }
-const state = reactive({
+
+interface ArchiveState {
+    currentJson: any,
+    series: {
+        name: string;
+        data: any[];
+    }[],
+    energy: number,
+    date: Date,
+    from: Date,
+    to: Date
+}
+
+const state: ArchiveState = reactive({
     currentJson: JSON.parse("{}"),
     series: [
         {
@@ -33,6 +58,9 @@ const options = {
     },
     xaxis: {
         type: "datetime",
+        labels : {
+            datetimeUTC: false,
+        }
     },
     yaxis: {
         title: { text: "Wm" },
@@ -79,12 +107,14 @@ updateChart();
     <div class="archive">
         <div class="form-floating mb-3">
             <label for="fromInput" color="black">from: </label>
-            <input type="datetime" class="form-control" id="fromInput" v-model="state.from" style="width:50%">
+            <input type="datetime-local" step="1" class="form-control" id="fromInput" :value="getDateString(state.from)"
+                @input="handleFromInput" style="width:50%">
 
         </div>
         <div class="form-floating mb-3">
             <label for="toInput">to: </label>
-            <input type="datetime" class="form-control" id="toInput" v-model="state.to" style="width:50%">
+            <input type="datetime-local" step="1" class="form-control" id="toInput" :value="getDateString(state.to)"
+                @input="handleToInput" style="width:50%">
         </div>
         <button @click="updateChart">Refresh</button>
         <apexchart :options="options" :series="state.series" type="line" height="300" />
